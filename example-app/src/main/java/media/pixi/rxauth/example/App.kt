3f190d2f35b4aa12199627b6e61c71e4dbd186e5
Nate Wickstrom <nwickstrom@pandora.com>
2019-01-20 12:41:09 -0800

    create auth provider

--- example-app/src/main/java/media/pixi/rxauth/example/App.kt
+++ example-app/src/main/java/media/pixi/rxauth/example/App.kt
@@ -2,10 +2,15 @@ package media.pixi.rxauth.example
 
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import media.pixi.rx.firebase.auth.kit.data.AuthProvider
 import media.pixi.rxauth.example.di.DaggerAppComponent
 import timber.log.Timber
+import javax.inject.Inject
 
 class App : DaggerApplication() {
+
+    @Inject
+    internal var authProvider: AuthProvider? = null
 
     override fun onCreate() {
         super.onCreate()
@@ -17,10 +22,19 @@ class App : DaggerApplication() {
             // TODO Timber.plant(new CrashlyticsTree());
         }
 
+        authProvider!!.observerAuthState()
+            .filter { result -> result }
+            .subscribe(
+                { t -> onLogout() },
+                { Timber.e(it) }
+            )
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().application(this).build()
     }
 
+    private fun onLogout() {
+        //tasksRepository.deleteAllTasks()
+    }
 }
--- example-app/src/main/java/media/pixi/rxauth/example/ui/SplashActivity.kt
+++ example-app/src/main/java/media/pixi/rxauth/example/ui/SplashActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import media.pixi.rxauth.example.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
