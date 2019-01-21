bea5fe5819ed6ef32e0a578beed82563151e60d4
Nate Wickstrom <nwickstrom@pandora.com>
2019-01-20 17:05:16 -0800

    fix xml crash

--- rx-firebase-auth-kit/src/main/java/media/pixi/rx/firebase/auth/kit/ui/signin/SigninActivity.kt
+++ rx-firebase-auth-kit/src/main/java/media/pixi/rx/firebase/auth/kit/ui/signin/SigninActivity.kt
@@ -1,7 +1,10 @@
 package media.pixi.rx.firebase.auth.kit.ui.signin
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_signin.*
 import media.pixi.rx.firebase.auth.kit.R
 import media.pixi.rx.firebase.auth.kit.data.AuthProvider
 import javax.inject.Inject
@@ -14,5 +17,23 @@ class SigninActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
+
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_signin -> {
+                //presenter.onLogout()
+                return true
+            }
+        }
+
+        return false
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.signin_menu, menu)
+        return true
     }
 }
