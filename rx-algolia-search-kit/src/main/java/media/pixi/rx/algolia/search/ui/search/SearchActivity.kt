e004ac288966081a67dcc7c1334f680ea441f9d0
Nate Wickstrom <nwickstrom@pandora.com>
2019-02-03 13:08:05 -0800

    remove databinding algolia

--- rx-algolia-search-kit/src/main/java/media/pixi/rx/algolia/search/ui/search/SearchActivity.kt
+++ rx-algolia-search-kit/src/main/java/media/pixi/rx/algolia/search/ui/search/SearchActivity.kt
@@ -1,13 +1,9 @@
 package media.pixi.rx.algolia.search.ui.search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import com.algolia.instantsearch.core.helpers.Searcher
-import com.algolia.instantsearch.ui.helpers.InstantSearch
-import com.algolia.instantsearch.ui.viewmodels.SearchBoxViewModel
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.search__activity_search.*
-import kotlinx.android.synthetic.main.search__searchbox.*
 import media.pixi.common.ActivityUtils
 import media.pixi.rx.algolia.search.R
 import media.pixi.rx.algolia.search.data.SearchProvider
