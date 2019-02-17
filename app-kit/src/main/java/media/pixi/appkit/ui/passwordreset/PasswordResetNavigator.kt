983b42c835e1dc5b7c84a7895937f839e477f1ba
Nate Wickstrom <nwickstrom@pandora.com>
2019-02-17 12:21:41 -0800

    open email provider from password reset

--- app-kit/src/main/java/media/pixi/appkit/ui/passwordreset/PasswordResetNavigator.kt
+++ app-kit/src/main/java/media/pixi/appkit/ui/passwordreset/PasswordResetNavigator.kt
@@ -2,11 +2,16 @@ package media.pixi.appkit.ui.passwordreset
 
 import android.app.Activity
 import javax.inject.Inject
+import android.content.Intent
+import media.pixi.appkit.R
+
 
 class PasswordResetNavigator @Inject internal constructor(): PasswordResetContract.Navigator {
 
     override fun showEmail(activity: Activity) {
-
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_APP_EMAIL)
+        activity.startActivity(Intent.createChooser(intent, activity.getString(R.string.open_email)))
     }
 
 }
