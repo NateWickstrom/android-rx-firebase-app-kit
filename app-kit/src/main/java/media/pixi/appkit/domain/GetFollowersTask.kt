2614952f59c36ec36feff440d80484638cb7353e
Nathan Wickstrom <wick@fb.com>
2019-09-15 16:13:09 -0700

    create get followers task

---
+++ app-kit/src/main/java/media/pixi/appkit/domain/GetFollowersTask.kt
@@ -0,0 +1,28 @@
+package media.pixi.appkit.domain
+
+import io.reactivex.Single
+import media.pixi.appkit.data.followers.FollowersProvider
+import media.pixi.appkit.data.profile.UserProfile
+import media.pixi.appkit.data.profile.UserProfileProvider
+import javax.inject.Inject
+
+class GetFollowersTask @Inject constructor(private val userProfileProvider: UserProfileProvider,
+                                           private val followersProvider: FollowersProvider) {
+
+    fun getFollowersForUser(userId: String) : Single<List<UserProfile>> {
+        return followersProvider.getFollowers(userId)
+            .firstOrError()
+            .flatMap { toProfiles(it) }
+    }
+
+    fun getMyFollowers() : Single<List<UserProfile>> {
+        return followersProvider.getMyFollowers()
+            .firstOrError()
+            .flatMap { toProfiles(it) }
+    }
+
+    private fun toProfiles(userIds: List<String>) : Single<List<UserProfile>> {
+        val users = userIds.map { userProfileProvider.observerUserProfile(it).firstOrError() }
+        return Single.zipArray({ list -> list.map { it as UserProfile }}, users.toTypedArray())
+    }
+}
